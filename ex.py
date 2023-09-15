723fee210b3d6c37911a623c719bbce08de14ed7
SangDoo Nam <kexe009@hotmail.com>
2023-09-15 14:21:54 +0200

    finish ex.py

---
+++ ex.py
@@ -0,0 +1,28 @@
+"""Task: You are given a list of strings. Some of those strings may contain integers, 
+for example Digital Car33r Institute. Implement a method digit_filter that takes 
+a list of strings as an argument and only returns those strings that don't contain a number. """
+import re
+
+# l33t = ['Digital Car33r Institute', 'DCI', 'Digital', 'Career', 'Inst1tut3']
+
+# new = []
+# for i in l33t:
+#     if not re.search(r'\d', i ):
+
+#         new.append(i)
+
+# print(new)
+
+def digit_filter(list):
+    new = []
+    for i in list:
+        if not re.search(r'\d', i):
+            
+            new.append(i)
+            
+    print(new)
+    
+
+l33t = ['Digital Car33r Institute', 'DCI', 'Digital', 'Career', 'Inst1tut3']
+
+digit_filter(l33t)
